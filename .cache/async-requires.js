7db67b0196847c7081dfc5d466a3c276e1d72c1a
Weiann <stargazer.wang@gmail.com>
2020-02-20 21:08:15 -0800

    quick update

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-product-js": () => import("/Users/Weian/GitHub/hunggwo/hunggwo.github.io/src/templates/product.js" /* webpackChunkName: "component---src-templates-product-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/Weian/GitHub/hunggwo/hunggwo.github.io/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/Weian/GitHub/hunggwo/hunggwo.github.io/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("/Users/Weian/GitHub/hunggwo/hunggwo.github.io/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-index-js": () => import("/Users/Weian/GitHub/hunggwo/hunggwo.github.io/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -5,13 +5,13 @@ class Jumbotron extends Component {
   render() {
     const categories = this.props.items.map((item, index) => {
       return (
-        <a href={item.name} class="col-md-4 col-sm-6 shadow-sm" style={{ padding: 5}}>
+        <a href={item.name} class="col-md-4 col-sm-6" style={{ padding: 5}}>
           <div class="one-item" style={{
             backgroundImage: `url(/photos/${item.name}/1.jpg)`,
             backgroundPosition: `center`,
             backgroundSize: `cover`,
           }}>
-            <div className="one-item-name">{item.name}</div>\
+            <div className="one-item-name">{item.name}</div>
           </div>
         </a>
         
